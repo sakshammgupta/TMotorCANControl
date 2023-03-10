6a0d50b73858ca65136ef1bde08f9f8c36d293ba
Saksham Gupta <sakshammgupta@gmail.com>
2023-03-10 19:13:07 +0530

    Update check_motor_connection_mit_can.py

--- demos/mit_can/check_motor_connection_mit_can.py
+++ demos/mit_can/check_motor_connection_mit_can.py
@@ -1,12 +1,5 @@
-import os
-import sys
-cur_path=os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, cur_path+"/../..")
-
-from src.TMotorCANControl.mit_can import *
-
 # import TMotorCANControl.mit_can 
-# from TMotorCANControl.mit_can import TMotorManager_mit_can
+from TMotorCANControl.mit_can import TMotorManager_mit_can
 
 # CHANGE THESE TO MATCH YOUR DEVICE!
 Type = 'AK80-64'
