d12de101957f63bb442f9f9cf87ead61fb9e5152
Saksham Gupta <sakshammgupta@gmail.com>
2023-03-10 19:26:37 +0530

    idle servo

--- demos/servo_can/demo_idle_servo_can.py
+++ demos/servo_can/demo_idle_servo_can.py
@@ -7,7 +7,7 @@ path.append("/home/pi/TMotorCANControl/src/")
 from TMotorCANControl.servo_can import TMotorManager_servo_can
 import time
 
-with TMotorManager_servo_can(motor_type='AK80-9', motor_ID=0) as dev:
+with TMotorManager_servo_can(motor_type='AK80-64', motor_ID=1) as dev:
     
     loop = SoftRealtimeLoop(dt=0.005, report=True, fade=0.0)
     dev.enter_idle_mode()
